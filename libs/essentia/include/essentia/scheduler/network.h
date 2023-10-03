886166fcb6ebb31ef9485d2a4388f747354765a8
Roy Macdonald <macdonald.roy@gmail.com>
2023-10-02 22:43:02 -0300

    Updated libs

--- libs/essentia/include/essentia/scheduler/network.h
+++ libs/essentia/include/essentia/scheduler/network.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2016  Music Technology Group - Universitat Pompeu Fabra
+ * Copyright (C) 2006-2021  Music Technology Group - Universitat Pompeu Fabra
  *
  * This file is part of Essentia
  *
@@ -266,6 +266,13 @@ class Network {
   void clearExecutionNetwork();
 };
 
+
+/**
+* Delete the algorithm together with its all visible dependencies without 
+* creating a network object.
+*/
+void deleteNetwork(const streaming::Algorithm* algo);
+
 /**
  * Prints the fill state of all the buffers in the last created network.
  */
@@ -282,7 +289,6 @@ AlgoVector computeCompositeDependencies(const streaming::Algorithm* algo);
  * and also avoid doing any allocation while running the network.
  */
 void cacheDependencies(streaming::Algorithm* algo);
-
 
 /**
  * Returns the list of all algorithms which live inside of the given composite
--- libs/essentia/include/essentia/version.h
+++ libs/essentia/include/essentia/version.h
@@ -1,6 +1,6 @@
 
 #ifndef VERSION_H_
 #define VERSION_H_
-#define ESSENTIA_VERSION "2.1-dev"
-#define ESSENTIA_GIT_SHA "5bfe18e"
+#define ESSENTIA_VERSION "2.1-beta6-dev"
+#define ESSENTIA_GIT_SHA "v2.1_beta5-1093-g4c48bb1c-dirty"
 #endif /* VERSION_H_ */
